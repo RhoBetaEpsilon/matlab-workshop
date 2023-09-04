2c19f8d579cc27b061dcb1fe8653d29b68f45f7f
Ashe <molly.02.andrews@gmail.com>
2023-09-04 15:56:58 -0400

    Added .gitignore for .asv files and BasicRunner.m template

---
+++ BasicRunner.m
@@ -0,0 +1,11 @@
+%% Import your classes into the script
+
+%% Create instances of your classes here
+
+%% Using your BasicClass instance, set the position data to a value and then get its value
+
+%% Using you BasicPlotter instance, plot the step response data from MATLABworkshopData.mat
+
+%% Now plot the gradient of the step response --> step velocity
+
+%% Finally, plot step response and step velocity on the same graph
