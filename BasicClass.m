c4d6f1f1420672ff32ce87ea87cf24827b352e6f
Ashe <molly.02.andrews@gmail.com>
2023-09-04 15:26:37 -0400

    Started BasicClass.m template

---
+++ BasicClass.m
@@ -0,0 +1,5 @@
+%% Put your class definition here --> don't forget the handle
+
+    %% Put your class properties here --> make one for storing position data
+
+    %% Put your class methods here --> make a getter and a setter for position data
